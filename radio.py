704379e2b0a73b35fb97a5d77f6df036fb336eca
Harsh Lathwal <xeon.harsh@gmail.com>
2017-01-22 23:52:29 +0530

    why

--- radio.py
+++ radio.py
@@ -1,17 +1,13 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-# import gtk
 
-class MainWindow(Gtk.Window):
+class RadioButtonWindow(Gtk.Window):
+
     def __init__(self):
-        Gtk.Window.__init__(self, title="Driver Manager") #title
+        Gtk.Window.__init__(self, title="RadioButton Demo")
         self.set_border_width(10)
 
-        #self.notebook = Gtk.Notebook()
-        #self.set_default_size(500, 300)
-        #self.notebook.set_tab_pos(gtk.POS_TOP)
-        #self.add(self.notebook)
         hbox = Gtk.Box(spacing=6)
         self.add(hbox)
 
@@ -36,9 +32,7 @@ class MainWindow(Gtk.Window):
             state = "off"
         print("Button", name, "was turned", state)
 
-        #self.notebook.append_page(self.page1, Gtk.Label('Nvidia GPU'))
-
-win = MainWindow() # makes window
-win.connect("delete-event", Gtk.main_quit) #delete event to ensure that the application is terminated if we click on the x to close the window.
-win.show_all() # display window
-Gtk.main() #start the GTK+ processing loop which we quit when the window is closed
+win = RadioButtonWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
